24b262c65cc1c2f28bdcbc3a1800ce8aaa1d2314
dongyuanxin <yuanxin.me@gmail.com>
2019-02-07 22:18:43 +0800

    hotfix: prevent two repeat click

--- source/js/backTop.js
+++ source/js/backTop.js
@@ -2,31 +2,41 @@
   const backTopBtn = document.querySelector('#back-top-btn');
 
   const backTop = () => {
-    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop,
-      delay = 10,
+    const delay = 10, 
       time = 200;
+    let running = false;
 
-    if (scrollTop <= 10) {
-      document.documentElement.scrollTop = 0;
-      document.body.scrollTop = 0;
-      return;
-    }
+    return () => {
+      if(running) return;
+      running = true;
 
-    let step = Math.ceil(scrollTop * delay / time);
-
-    let timer = setInterval(() => {
-      scrollTop =
-        document.documentElement.scrollTop || document.body.scrollTop;
-      if (scrollTop - step <= 0) {
+      let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  
+      if(scrollTop <= 10) {
         document.documentElement.scrollTop = 0;
         document.body.scrollTop = 0;
-        clearInterval(timer);
-      } else {
-        document.documentElement.scrollTop = scrollTop - step;
-        document.body.scrollTop = scrollTop - step;
+        running = false;
+        return;
       }
-    }, delay);
+  
+      let step = Math.ceil(scrollTop * delay / time);
+  
+      let timer = setInterval(() => {
+        scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+        if(scrollTop <= step) {
+          document.documentElement.scrollTop = 0;
+          document.body.scrollTop = 0;
+          running = false;
+          clearInterval(timer);
+        } else {
+          document.documentElement.scrollTop = scrollTop - step;
+          document.body.scrollTop = scrollTop - step;
+        }
+      }, delay);
+    };
   };
 
-  backTopBtn.addEventListener('click', backTop, false);
+  backTopBtn.addEventListener('click', backTop(), false);
+  
 })();
