546d6dd011dc537ccc08c752a60667565b2d3de5
Yuanxin Dong <yuanxin.me@gmail.com>
2019-02-23 21:19:39 +0800

    add: toc dom and javascript

--- source/js/passage.js
+++ source/js/passage.js
@@ -11,4 +11,26 @@
           .querySelectorAll('img')
           .forEach(image => image.addEventListener('click', handleImgClick))
     );
+
+  const layer = document.querySelector('#site-layer'),
+    layerContent = layer.querySelector('.site-layer-content'),
+    toc = document.querySelector('#site-toc'),
+    tocShowBtn = document.querySelector('#site-toc-show-btn'),
+    tocHideBtn = document.querySelector('#site-toc-hide-btn');
+
+  tocShowBtn
+    .addEventListener('click', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      layer.style.display = 'block';
+      layerContent.style.display = 'none';
+      toc.style.right = '0';
+    });
+  
+  tocHideBtn
+    .addEventListener('click', (e) => {
+      toc.style.right = '';
+      layer.style.display = 'none';
+      layerContent.style.display = '';
+    });
 })();
